c8318d04b56bb53e3f7744ea2a00eb55dd6e5a5f
chasesunshine <1061902084@qq.com>
2023-09-18 13:44:01 +0800

    设计模式 - 观察者模式（个人对代码层级进行一些修改）

---
+++ msb-observer-pattern-12/src/main/java/com/mashibing/observer/example01/Subject.java
@@ -0,0 +1,13 @@
+package com.mashibing.observer.example01;
+
+/**
+ * 抽象目标类 - 抽象被观察者
+ * @author spikeCong
+ * @date 2022/10/11
+ **/
+public interface Subject {
+
+    void attach(Observer observer);
+    void detach(Observer observer);
+    void notifyObservers();
+}
