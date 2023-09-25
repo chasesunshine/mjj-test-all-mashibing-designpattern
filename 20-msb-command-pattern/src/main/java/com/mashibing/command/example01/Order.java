32b943b8b58432cb32c487aff57c95d336d16fd1
马佳健 <majiajian@dongfangfuli.com>
2023-09-26 00:22:46 +0800

    设计模式总结

--- 20-msb-command-pattern/src/main/java/com/mashibing/command/example01/Order.java
+++ 20-msb-command-pattern/src/main/java/com/mashibing/command/example01/Order.java
@@ -1,5 +1,7 @@
 package com.mashibing.command.example01;
 
+import lombok.Data;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,25 +10,16 @@ import java.util.Map;
  * @author spikeCong
  * @date 2022/10/20
  **/
+@Data
 public class Order {
 
-    private int diningTable;  //餐桌号码
+    /**
+     * 餐桌号码
+     */
+    private int diningTable;
 
-    private Map<String,Integer> foodMenu = new HashMap<>();  //存储菜名和份数
-
-    public int getDiningTable() {
-        return diningTable;
-    }
-
-    public void setDiningTable(int diningTable) {
-        this.diningTable = diningTable;
-    }
-
-    public Map<String, Integer> getFoodMenu() {
-        return foodMenu;
-    }
-
-    public void setFoodMenu(Map<String, Integer> foodMenu) {
-        this.foodMenu = foodMenu;
-    }
+    /**
+     * 存储菜名和份数
+     */
+    private Map<String,Integer> foodMenu = new HashMap<>();
 }
